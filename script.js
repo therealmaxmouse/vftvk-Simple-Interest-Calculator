2b4f7e316b35f37b8ce19577659c68e703fc9dd2
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-07-29 23:10:00 -0400

    adding focus on amount after alert

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function compute()
 
     if (principal <= 0){
         alert("Enter a positive number");
+        document.getElementById("principal").focus();
     }
     else {
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+initialrate+"%\</mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>";
