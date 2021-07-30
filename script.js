85bd8d7147bc0eebcb2db3bfbba76a9f5e1ba824
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-07-29 22:15:22 -0400

    updating script.js
    for interest calculator requirements

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var initialrate = document.getElementById("rate").defaultValue;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * initialrate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+initialrate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
     
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+    document.getElementById("rate").defaultValue=rateval; 
+}
