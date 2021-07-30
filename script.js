5896e7655fa0657164c1909d5e1024f90feef207
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-07-29 22:55:00 -0400

    updating
    adding amount validation and result highlighting

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * initialrate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+initialrate+"%.\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+initialrate+"%\</mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>";
     
 }
 
@@ -14,4 +14,12 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval + "%";
     document.getElementById("rate").defaultValue=rateval; 
+}
+
+function validateAmount()
+{
+    var amount = document.getElementById("principal").value;
+    if (amount <= 0){
+        alert("Enter a positive number");
+    }
 }
